6e4c3ebc3380b0474d4c8d17a0d13de8be171da1
cjgleason <cjgleason@umass.edu>
2026-03-26 13:18:01 -0400

    Create Busboi.py

---
+++ output/modules/Busboi.py
@@ -0,0 +1,61 @@
+# Standard imports
+import glob
+from pathlib import Path
+
+# Third-party imports
+from netCDF4 import Dataset
+import numpy as np
+
+# Local imports
+from output.modules.AbstractModule import AbstractModule
+
+class Busboi(AbstractModule):
+    """
+    A class that represents the results of running BUSBOI.
+
+    Data and operations append BUSBOI results to the SoS on the appropriate
+    dimensions.
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    append_module_data(data_dict)
+        append module data to the new version of the SoS result file.
+    create_data_dict()
+        creates and returns module data dictionary.
+    get_module_data()
+        retrieve module results from NetCDF files.
+    get_nc_attrs(nc_file, data_dict)
+        get NetCDF attributes for each NetCDF variable.
+    """
+
+    def __init__(self, cont_ids, input_dir, sos_new, logger, vlen_f, vlen_i, vlen_s,
+                 rids, nrids, nids):
+        """
+        Parameters
+        ----------
+        cont_ids: list
+            list of continent identifiers
+        input_dir: Path
+            path to input directory
+        sos_new: Path
+            path to new SOS file
+        logger: logging.Logger
+            logger to log statements with
+        vlen_f: VLType
+            variable length float data type for NetCDF ragged arrays
+        vlen_i: VLType
+            variable length int data type for NetCDF ragged arrays
+        vlen_s: VLType
+            variable length string data type for NetCDF ragged arrays
+        rids: nd.array
+            array of SoS reach identifiers associated with continent
+        nrids: nd.array
+            array of SOS reach identifiers on the node-level
+        nids: nd.array
+            array of SOS node identifiers
+        """
+
+        super().__init__(cont_ids, input_dir, sos_new, logger, vlen_f,
