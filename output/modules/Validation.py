c65834f07325099f827a3ca23dbd373348ded92b
Steve Coss <coss.31@osu.edu>
2026-08-23 18:26:34 -0400

    Update Validation.py

--- output/modules/Validation.py
+++ output/modules/Validation.py
@@ -155,8 +155,9 @@ class Validation(AbstractModule):
             }
 
             data_dict[group]["algo_names"] = np.full((num_algos_dim, self.nchar), '')
-            print(algo_names)
+           
             for i, name in enumerate(algo_names):
+                print(name)
                 data_dict[group]['algo_names'][i, :len(name)] = list(name)
 
         data_dict['flpe']['time'] = np.empty((self.sos_rids.shape[0]), dtype=object)
