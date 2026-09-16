1a5ef11ac2a167f18d8a54c5accc8a7d234256ee
scoss <coss.31@osu.edu>
2026-09-16 12:01:11 -0400

    DIM

--- output/modules/Validation.py
+++ output/modules/Validation.py
@@ -205,15 +205,18 @@ class Validation(AbstractModule):
         sos_ds    = Dataset(self.sos_new, 'a')
         val_t_grp = sos_ds.createGroup("validation")
 
-        #dump calibration gauge info directly into the val group
-        var = self.write_var(val_t_grp, "moi_gauge_status", "i4", ("num_reaches",),self.reach_con_status)
-        var = self.write_var(val_t_grp, "moi_gauge_validation", "i4", ("num_reaches",),self.reach_con_validation)
-        var = self.write_var(val_t_grp, "moi_gauge_calibration", "i4", ("num_reaches",),self.reach_con_calibration)
+        
 
         val_t_grp.createDimension("num_algos", self.num_algos)
         val_t_grp.createDimension("num_algos_offline", self.num_algos_offline)
         val_t_grp.createDimension("nchar", self.nchar)
         val_t_grp.createDimension("num_reaches", self.sos_rids.shape[0])
+
+        #dump calibration gauge info directly into the val group
+        var = self.write_var(val_t_grp, "moi_gauge_status", "i4", ("num_reaches",),self.reach_con_status)
+        var = self.write_var(val_t_grp, "moi_gauge_validation", "i4", ("num_reaches",),self.reach_con_validation)
+        var = self.write_var(val_t_grp, "moi_gauge_calibration", "i4", ("num_reaches",),self.reach_con_calibration)
+        
 
         for group in self.out_groups:
             val_grp = val_t_grp.createGroup(group)
